fa7c544074253da71558bfb5f4dd2a1d20099a42
Minug0226 <85049368+minug0226@users.noreply.github.com>
2025-01-22 03:05:43 +0900

    Update main.tf
    리팩터링

--- main.tf
+++ main.tf
@@ -10,20 +10,21 @@ terraform {
 }
 
 provider "openstack" {
-  # clouds.yaml의 cloud 이름
-  cloud = "openstack"
-  # 예: 내부 TLS 인증서를 무시하려면 추가
-  # insecure = true
+  # variables.tf에서 받아온 값 사용
+  cloud    = var.openstack_cloud
+  insecure = var.openstack_insecure
 }
 
-# 1. 보안그룹: inbound, outbound 모든 프로토콜 오픈
+########################
+# 1. 보안그룹
+########################
+
 resource "openstack_networking_secgroup_v2" "allow_icmp_tcp_udp_sg" {
   name        = "allow-icmp-tcp-udp"
   description = "Allow all inbound/outbound ICMP, TCP, UDP"
 }
 
-
-# 1) ICMP in
+# Ingress ICMP
 resource "openstack_networking_secgroup_rule_v2" "icmp_in" {
   direction         = "ingress"
   ethertype         = "IPv4"
@@ -32,7 +33,7 @@ resource "openstack_networking_secgroup_rule_v2" "icmp_in" {
   security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
 }
 
-# 2) ICMP out
+# Egress ICMP
 resource "openstack_networking_secgroup_rule_v2" "icmp_out" {
   direction         = "egress"
   ethertype         = "IPv4"
@@ -41,7 +42,7 @@ resource "openstack_networking_secgroup_rule_v2" "icmp_out" {
   security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
 }
 
-# 3) TCP in
+# Ingress TCP
 resource "openstack_networking_secgroup_rule_v2" "tcp_in" {
   direction         = "ingress"
   ethertype         = "IPv4"
@@ -52,7 +53,7 @@ resource "openstack_networking_secgroup_rule_v2" "tcp_in" {
   security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
 }
 
-# 4) TCP out
+# Egress TCP
 resource "openstack_networking_secgroup_rule_v2" "tcp_out" {
   direction         = "egress"
   ethertype         = "IPv4"
@@ -63,7 +64,7 @@ resource "openstack_networking_secgroup_rule_v2" "tcp_out" {
   security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
 }
 
-# 5) UDP in
+# Ingress UDP
 resource "openstack_networking_secgroup_rule_v2" "udp_in" {
   direction         = "ingress"
   ethertype         = "IPv4"
@@ -74,7 +75,7 @@ resource "openstack_networking_secgroup_rule_v2" "udp_in" {
   security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
 }
 
-# 6) UDP out
+# Egress UDP
 resource "openstack_networking_secgroup_rule_v2" "udp_out" {
   direction         = "egress"
   ethertype         = "IPv4"
@@ -85,52 +86,61 @@ resource "openstack_networking_secgroup_rule_v2" "udp_out" {
   security_group_id = openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
 }
 
-# 3. k8s-external 네트워크/서브넷
+########################
+# 2. k8s-external 네트워크/서브넷
+########################
+
 resource "openstack_networking_network_v2" "k8s_external_net" {
-  name           = "k8s-external"
+  name           = var.k8s_external_net_name
   admin_state_up = true
 }
 
 resource "openstack_networking_subnet_v2" "k8s_external_subnet" {
-  name            = "k8s-external-subnet"
+  name            = var.k8s_external_subnet_name
   network_id      = openstack_networking_network_v2.k8s_external_net.id
-  cidr            = "10.10.10.0/24"
+  cidr            = var.k8s_external_cidr
   ip_version      = 4
-  gateway_ip      = "10.10.10.254"  # 외부 통신 게이트웨이
+  gateway_ip      = var.k8s_external_gateway_ip
   enable_dhcp     = true
-  dns_nameservers = ["8.8.8.8"]
+  dns_nameservers = var.k8s_external_dns
 }
 
+########################
+# 3. k8s-internal 네트워크/서브넷
+########################
 
-# 2. k8s-internal 네트워크/서브넷
 resource "openstack_networking_network_v2" "k8s_internal_net" {
-  name           = "k8s-internal"
+  name           = var.k8s_internal_net_name
   admin_state_up = true
 }
 
 resource "openstack_networking_subnet_v2" "k8s_internal_subnet" {
-  name            = "k8s-internal-subnet"
+  name            = var.k8s_internal_subnet_name
   network_id      = openstack_networking_network_v2.k8s_internal_net.id
-  cidr            = "192.168.0.0/16"
+  cidr            = var.k8s_internal_cidr
   ip_version      = 4
-  gateway_ip      = null           # 게이트웨이 비활성 (null)
-  enable_dhcp     = true           # 원하는 경우 DHCP 활성/비활성 선택
+  # var.k8s_internal_gateway_ip가 빈 문자열("")이면 gateway_ip를 null로 처리
+  gateway_ip      = length(var.k8s_internal_gateway_ip) > 0 ? var.k8s_internal_gateway_ip : null
+  enable_dhcp     = var.k8s_enable_dhcp
 }
 
+########################
+# 키 페어
+########################
 
 resource "openstack_compute_keypair_v2" "auto_gen_key" {
   name = "tofu-key"
-  // 공개키를 입력하지 않으면 Terraform이 자동으로 키 페어를 생성합니다.
+  # 공개키를 입력하지 않으면 Terraform이 자동 생성
 }
 
-# Private Key를 출력 (민감 데이터 처리)
+# Private Key 출력 (민감데이터)
 output "generated_private_key" {
   description = "This is the newly generated private key from OpenStack"
   value       = openstack_compute_keypair_v2.auto_gen_key.private_key
   sensitive   = true
 }
 
-# Public Key를 출력
+# Public Key 출력
 output "generated_public_key" {
   description = "This is the newly generated public key from OpenStack"
   value       = openstack_compute_keypair_v2.auto_gen_key.public_key
@@ -138,16 +148,19 @@ output "generated_public_key" {
 
 # Private Key를 로컬 파일로 저장
 resource "local_file" "my_private_key_file" {
-  content          = openstack_compute_keypair_v2.auto_gen_key.private_key
-  filename         = "./tofu-key.pem"
-  file_permission  = "400"
+  content         = openstack_compute_keypair_v2.auto_gen_key.private_key
+  filename        = "./tofu-key.pem"
+  file_permission = "400"
 }
 
+########################
+# Infra Port들
+########################
 
 resource "openstack_networking_port_v2" "infra_port_controller" {
   name       = "infra-port-controller"
-  network_id = "6c07f67e-1ea4-4726-bf6b-3a83bebb49ff"
-  
+  network_id = var.infra_network_id
+
   security_group_ids = [
     openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
   ]
@@ -155,8 +168,8 @@ resource "openstack_networking_port_v2" "infra_port_controller" {
 
 resource "openstack_networking_port_v2" "infra_port_compute1" {
   name       = "infra-port-compute1"
-  network_id = "6c07f67e-1ea4-4726-bf6b-3a83bebb49ff"
-  
+  network_id = var.infra_network_id
+
   security_group_ids = [
     openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
   ]
@@ -164,8 +177,8 @@ resource "openstack_networking_port_v2" "infra_port_compute1" {
 
 resource "openstack_networking_port_v2" "infra_port_compute2" {
   name       = "infra-port-compute2"
-  network_id = "6c07f67e-1ea4-4726-bf6b-3a83bebb49ff"
-  
+  network_id = var.infra_network_id
+
   security_group_ids = [
     openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
   ]
@@ -173,250 +186,251 @@ resource "openstack_networking_port_v2" "infra_port_compute2" {
 
 resource "openstack_networking_port_v2" "infra_port_storage" {
   name       = "infra-port-storage"
-  network_id = "6c07f67e-1ea4-4726-bf6b-3a83bebb49ff"
-  
+  network_id = var.infra_network_id
+
   security_group_ids = [
     openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
   ]
 }
 
-resource "openstack_networking_port_v2" "infra_port_win_vm" {
-  name       = "infra-port-win-vm"
-  network_id = "6c07f67e-1ea4-4726-bf6b-3a83bebb49ff"
-  
+resource "openstack_networking_port_v2" "infra_port_ansible_server" {
+  name       = "infra-port-ansible_server"
+  network_id = var.infra_network_id
+
   security_group_ids = [
     openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
   ]
 }
 
-
-
+########################
+# K8s external / internal Port
+########################
 
 resource "openstack_networking_port_v2" "k8s_external_port_1" {
   name       = "k8s-external-port-1"
   network_id = openstack_networking_network_v2.k8s_external_net.id
-  
-  security_group_ids = [ openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id ]
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.k8s_external_subnet.id
-    ip_address = "10.10.10.10"
+    ip_address = var.k8s_external_port_1_ip
   }
 }
 
 resource "openstack_networking_port_v2" "k8s_external_port_2" {
   name       = "k8s-external-port-2"
   network_id = openstack_networking_network_v2.k8s_external_net.id
-  
-  security_group_ids = [ openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id ]
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.k8s_external_subnet.id
-    ip_address = "10.10.10.20"
+    ip_address = var.k8s_external_port_2_ip
   }
 }
 
 resource "openstack_networking_port_v2" "k8s_external_port_3" {
   name       = "k8s-external-port-3"
   network_id = openstack_networking_network_v2.k8s_external_net.id
-  
-  security_group_ids = [ openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id ]
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.k8s_external_subnet.id
-    ip_address = "10.10.10.30"
+    ip_address = var.k8s_external_port_3_ip
   }
 }
 
 resource "openstack_networking_port_v2" "k8s_internal_port_1" {
   name       = "k8s_internal_port_1"
   network_id = openstack_networking_network_v2.k8s_internal_net.id
-  
-  security_group_ids = [ openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id ]
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.k8s_internal_subnet.id
-    ip_address = "192.168.10.100"
+    ip_address = var.k8s_internal_port_1_ip
   }
 }
 
 resource "openstack_networking_port_v2" "k8s_internal_port_2" {
   name       = "k8s_internal_port_2"
   network_id = openstack_networking_network_v2.k8s_internal_net.id
-  
-  security_group_ids = [ openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id ]
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.k8s_internal_subnet.id
-    ip_address = "192.168.10.10"
+    ip_address = var.k8s_internal_port_2_ip
   }
 }
 
 resource "openstack_networking_port_v2" "k8s_internal_port_3" {
   name       = "k8s_internal_port_3"
   network_id = openstack_networking_network_v2.k8s_internal_net.id
-  
-  security_group_ids = [ openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id ]
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.k8s_internal_subnet.id
-    ip_address = "192.168.10.20"
+    ip_address = var.k8s_internal_port_3_ip
   }
 }
 
 resource "openstack_networking_port_v2" "k8s_internal_port_4" {
   name       = "k8s_internal_port_4"
   network_id = openstack_networking_network_v2.k8s_internal_net.id
-  
-  security_group_ids = [ openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id ]
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.k8s_internal_subnet.id
-    ip_address = "192.168.10.30"
+    ip_address = var.k8s_internal_port_4_ip
   }
 }
 
 resource "openstack_networking_port_v2" "k8s_internal_port_5" {
   name       = "k8s_internal_port_5"
   network_id = openstack_networking_network_v2.k8s_internal_net.id
-  
-  security_group_ids = [ openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id ]
+  security_group_ids = [
+    openstack_networking_secgroup_v2.allow_icmp_tcp_udp_sg.id
+  ]
   fixed_ip {
     subnet_id  = openstack_networking_subnet_v2.k8s_internal_subnet.id
-    ip_address = "192.168.10.40"
+    ip_address = var.k8s_internal_port_5_ip
   }
 }
 
 ########################
-# 1) ansible
+# 1) Ansible Server
 ########################
-resource "openstack_compute_instance_v2" "win_vm" {
-  name           = "ansible-server"
-  flavor_name    = "t1.k8s"  # 실제 존재하는 Flavor
-  image_id       = "7666e39a-b7c4-4cd1-b10b-2e3cb28fc221"  # Windows10 이미지 ID
-  key_pair       = openstack_compute_keypair_v2.auto_gen_key.name
+resource "openstack_compute_instance_v2" "ansible_server" {
+  name        = var.ansible_server_name
+  flavor_name = var.openstack_flavor_name
+  image_id    = var.openstack_image_id
+  key_pair    = openstack_compute_keypair_v2.auto_gen_key.name
 
   network {
-    port = openstack_networking_port_v2.infra_port_win_vm.id
+    port = openstack_networking_port_v2.infra_port_ansible_server.id
   }
 
-  # 원하는 포트 연결 (예: 내부 + 외부)
   network {
     port = openstack_networking_port_v2.k8s_internal_port_1.id
   }
+
   network {
     port = openstack_networking_port_v2.k8s_external_port_1.id
   }
 
-  user_data = templatefile("${path.module}/cloud_init.tpl", {
-    public_key = openstack_compute_keypair_v2.auto_gen_key.public_key
-  })
+  user_data = templatefile(
+    "${path.module}/${var.cloud_init_template}",
+    { public_key = openstack_compute_keypair_v2.auto_gen_key.public_key }
+  )
 
-  config_drive = true  # Config Drive 활성화
+  config_drive = true
 }
 
 ########################
 # 2) Controller Node
 ########################
 resource "openstack_compute_instance_v2" "controller_node" {
-  name          = "controller-node"
-  flavor_name   = "t1.k8s"
-  image_id      = "7666e39a-b7c4-4cd1-b10b-2e3cb28fc221"
-  key_pair      = openstack_compute_keypair_v2.auto_gen_key.name
+  name        = var.controller_node_name
+  flavor_name = var.openstack_flavor_name
+  image_id    = var.openstack_image_id
+  key_pair    = openstack_compute_keypair_v2.auto_gen_key.name
 
   network {
-    # 위에서 만든 Port
     port = openstack_networking_port_v2.infra_port_controller.id
   }
-  # 내부망 NIC만 연결
+
   network {
     port = openstack_networking_port_v2.k8s_internal_port_2.id
   }
 
-  user_data = templatefile("${path.module}/cloud_init.tpl", {
+  user_data = templatefile(var.cloud_init_template, {
     public_key = openstack_compute_keypair_v2.auto_gen_key.public_key
   })
 
-  config_drive = true  # Config Drive 활성화
-
+  config_drive = true
 }
 
 ########################
 # 3) Compute Node1
 ########################
 resource "openstack_compute_instance_v2" "compute_node1" {
-  name          = "compute-node1"
-  flavor_name   = "t1.k8s"
-  image_id      = "7666e39a-b7c4-4cd1-b10b-2e3cb28fc221"
-  key_pair      = openstack_compute_keypair_v2.auto_gen_key.name
+  name        = var.compute_node1_name
+  flavor_name = var.openstack_flavor_name
+  image_id    = var.openstack_image_id
+  key_pair    = openstack_compute_keypair_v2.auto_gen_key.name
 
   network {
     port = openstack_networking_port_v2.infra_port_compute1.id
   }
 
-  # 내부망 NIC
   network {
     port = openstack_networking_port_v2.k8s_internal_port_3.id
   }
 
-  # 외부망 NIC
   network {
     port = openstack_networking_port_v2.k8s_external_port_2.id
   }
 
-  user_data = templatefile("${path.module}/cloud_init.tpl", {
+  user_data = templatefile(var.cloud_init_template, {
     public_key = openstack_compute_keypair_v2.auto_gen_key.public_key
   })
 
-  config_drive = true  # Config Drive 활성화
+  config_drive = true
 }
 
 ########################
 # 4) Compute Node2
 ########################
 resource "openstack_compute_instance_v2" "compute_node2" {
-  name          = "compute-node2"
-  flavor_name   = "t1.k8s"
-  image_id      = "7666e39a-b7c4-4cd1-b10b-2e3cb28fc221"
-  key_pair      = openstack_compute_keypair_v2.auto_gen_key.name
-
+  name        = var.compute_node2_name
+  flavor_name = var.openstack_flavor_name
+  image_id    = var.openstack_image_id
+  key_pair    = openstack_compute_keypair_v2.auto_gen_key.name
 
   network {
     port = openstack_networking_port_v2.infra_port_compute2.id
   }
 
-  # 내부망 NIC
   network {
     port = openstack_networking_port_v2.k8s_internal_port_4.id
   }
 
-  # 외부망 NIC
   network {
     port = openstack_networking_port_v2.k8s_external_port_3.id
   }
 
-  user_data = templatefile("${path.module}/cloud_init.tpl", {
+  user_data = templatefile(var.cloud_init_template, {
     public_key = openstack_compute_keypair_v2.auto_gen_key.public_key
   })
 
-  config_drive = true  # Config Drive 활성화
+  config_drive = true
 }
 
 ########################
 # 5) Storage Node
 ########################
 resource "openstack_compute_instance_v2" "storage_node" {
-  name          = "storage-node"
-  flavor_name   = "t1.k8s"
-  image_id      = "7666e39a-b7c4-4cd1-b10b-2e3cb28fc221"
-  key_pair      = openstack_compute_keypair_v2.auto_gen_key.name
+  name        = var.storage_node_name
+  flavor_name = var.openstack_flavor_name
+  image_id    = var.openstack_image_id
+  key_pair    = openstack_compute_keypair_v2.auto_gen_key.name
 
   network {
     port = openstack_networking_port_v2.infra_port_storage.id
   }
 
-  # 내부망 NIC만 연결
   network {
     port = openstack_networking_port_v2.k8s_internal_port_5.id
   }
 
-  user_data = templatefile("${path.module}/cloud_init.tpl", {
+  user_data = templatefile(var.cloud_init_template, {
     public_key = openstack_compute_keypair_v2.auto_gen_key.public_key
   })
 
-  config_drive = true  # Config Drive 활성화
+  config_drive = true
 }
-
